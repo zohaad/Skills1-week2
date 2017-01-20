6edbb61c1ebf599e0f3a5dcfbeaf32a293cbc8ff
Zohaad Fazal <zohaad.fazal@gmail.com>
2017-01-20 22:48:52 +0100

    made simpleSort

--- cell.java
+++ cell.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
 public class cell {
-  private List<Integer> hints;
+  private ArrayList<Integer> hints;
+  private int[] position;
 
-  public cell (int[] input) { // constuctor
+  public cell (ArrayList<Integer> input, int[] pos) { // constuctor
     this.hints = new ArrayList<Integer>();
-    for (int i = 0; i < input.length; i++) {  
-        this.hints.add(input[i]);
+    for (int i = 0; i < input.size(); i++) {  
+        this.hints.add(input.get(i));
     }
+    this.position = new int[2];
+    this.position[0] = pos[0];
+    this.position[1] = pos[1];
   }
 
   public void remove (int i) {
-    this.hints.remove(Integer.valueOf(i));
+    if (contains(i)) this.hints.remove(Integer.valueOf(i));
   }
 
   public boolean contains (int i) {
@@ -19,8 +23,29 @@ public class cell {
     else return false;
   }
 
-  public int size() {
-      return this.hints.size();
+  public int size () {
+    return this.hints.size();
+  }
+
+  public int[] position () {
+    return this.position;
+  }
+
+  public int row () {
+    return this.position[0];
+  }
+
+  public int col () {
+    return this.position[1];
+  }
+
+  public ArrayList<Integer> currentHints () {
+    return this.hints;
+  }
+
+  // VERY DANGEROUS
+  public int solution() {
+    return this.hints.get(0);
   }
 
   
--- matrix.java
+++ matrix.java
@@ -1,25 +1,83 @@
+import java.util.*;
+
 public class matrix {
   private cell[][] cellMatrix;
-  private Stack queue;
+  private Queue<cell> queue; // a nice FIFO structure
 
-  public matrix (cell[] givenHints) { // constructor
-    int[] defaultInput = new int[9];
+  public matrix (cell[] givenCells) { // constructor
+    // making the default input
+    ArrayList<Integer> defaultInput = new ArrayList<Integer>();
     for (int i = 0; i < 9; i++) {
-        defaultInput[i] = i + 1;
+        defaultInput.add(i + 1); // 1 ... 9
     }
+
     this.cellMatrix = new cell[9][9];
+    int position[] = new int[2];
     for (int i = 0; i < 9; i++) {
+      position[0] = i;
       for (int j = 0; j < 9; j++) {
-        this.cellMatrix[i][j] = new cell(defaultInput);
+        position[1] = j;
+        this.cellMatrix[i][j] = new cell(defaultInput, position);
       }
     }
-    for (int i = 0; i < givenHints.length; i++) { // making the queue filled with stacks
-      this.queue.push(givenHints[i]);
+    queue = new LinkedList<cell>();
+    for (int i = 0; i < givenCells.length; i++) {
+      this.queue.add(givenCells[i]); // making the queue filled with cells
+      replace(givenCells[i]); // replace the matrix entries
     }
   }
 
+  public void replace(cell myCell) { 
+    this.cellMatrix[myCell.position()[0]][myCell.position()[1]] = new cell(myCell.currentHints(), myCell.position());
+  }
 
-  public void replace(int[] entry, cell myCell) { 
-    this.cellMatrix[entry[0]][entry[1]] = new cell(myCell.)
+  public void simpleSolve () {
+    while (queue.size() < 81) {
+      cell currentCell = queue.poll(); // get first in queue and remove it
+      rowSolve(currentCell);
+      colSolve(currentCell);
+      blockSolve(currentCell);
+      queue.add(currentCell); // add as last in queue
+    }
+  }
+
+  public void rowSolve (cell myCell) {
+    cell x;
+    for (int i = 0; i < 9; i++) {
+      x = cellMatrix[myCell.row()][i];
+      if (i != myCell.col()) {
+        x.remove(myCell.solution());
+        if (x.size() == 1) queue.add(x); // if size is 1, add to the queue
+      }
+    }
+  }
+
+  public void colSolve (cell myCell) {
+    cell x;
+    for (int i = 0; i < 9; i++) {
+      x = cellMatrix[i][myCell.col()];
+      if (i != myCell.row()) {
+        x.remove(myCell.solution());
+        if (x.size() == 1) queue.add(x);
+      }
+    }
+  }
+
+  public void blockSolve (cell myCell) {
+    cell x;
+    // figure out a starting point
+    int[] start_point = new int[2];
+    start_point[0] = myCell.row();
+    start_point[1] = myCell.col();
+    while (start_point[0] != 0 || start_point[0] != 3 || start_point[0] != 6) start_point[0]--;
+    while (start_point[1] != 0 || start_point[1] != 3 || start_point[1] != 6) start_point[1]--;
+    for (int i = start_point[0]; i < start_point[0] + 3; i++) {
+      for (int j = start_point[1]; j < start_point[1] + 3; j++) {
+        if (!(i == myCell.row() && j == myCell.col())){
+          cellMatrix[i][j].remove(myCell.solution());
+          if (cellMatrix[i][j].size() == 1) queue.add(cellMatrix[i][j]);
+        }
+      }
+    }
   }
 }
